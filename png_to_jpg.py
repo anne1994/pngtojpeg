06f5451d8402dd6daeaf35269f03f67ea8582b0d
anne1994 <anneangelina94@gmail.com>
2017-07-20 00:15:19 -0700

    Update png_to_jpg.py

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -4,10 +4,10 @@ import os
 from os import listdir, makedirs
 from os.path import isdir, join, exists
 from shutil import copyfile
-import scipy
-from scipy.misc import imread
+#import scipy
+#from scipy.misc import imread
 import cv2
-import matplotlib as plt
+#import matplotlib as plt
 
 print("ho")
 curate_dir = "C:/Users/annea/tf_files/homework"
@@ -30,16 +30,6 @@ for label in labels:
     files = fnmatch.filter(listdir(cdir),'*.png')
     #nfiles = len(files)
     for f in files:
-        #pic = str(f)
-        #A=cv2.imread(files)
-        #im=Image.fromarray(A)
-        #im.save("f.jpg")
-        #scipy.misc.imsave("f.jpg")
-        #files = fnmatch.filter(listdir(cdir),'*.jpg')
-        #print(files)
-        #file,ext = os.path.splitext(f)
-        #print(file)
-        pic=f
         A=cv2.imread(os.path.join(cdir,f))
         im=Image.fromarray(A)
         img_path2="C:/Users/annea/tf_files/images/"+label
